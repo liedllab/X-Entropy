8aa955f8903cc7bc536a5752970cf587e05aa277
Patrick Quoika <Patrick.Quoika@uibk.ac.at>
2020-10-14 17:16:41 +0200

    continue design of dihedral entropy class
    Still missing the counterpart from the cython kernel for dihedrals with
    weights. So far the pipeline only accepts one data set. Would probably
    be a good idea to take out subroutines from the calculate functions, for
    them to be called in a loop of a collection of data sets. Didnt test
    anything, yet...
    
    
    Former-commit-id: f940ba2f5e82fef0429047d498e00542080d0232

--- entropy/dihedrals.py
+++ entropy/dihedrals.py
@@ -46,49 +46,41 @@ def preprocess_dihedral_data(data):
     return data
 
 
+def start_end_from_grid(grid):
+    return np.nanmin(grid), np.nanmax(grid)
+
+
 class dihedralEntropy(object):
     __bins = None
     __data = None
-    __weights= None
+    __weights = None
     __has_weights = None
     __resolution = None
-    __kde_done = False
     __successful = None
     __verbose = None
+    __method = None
     __pdf = None
     __pdf_x = None
     __bandwidth = None
-    __is_finished = None
+    __kde_is_calculated = None
+    __entropy_is_calculated = None
+    __entropies = None
 
-    def __init__(self, data, weights=None, resolution=4096, verbose=False):
+    def __init__(self, data, weights=None, resolution=4096, verbose=False, method="Simpson"):
         self.__data = preprocess_dihedral_data(data)
         weights, weight_switch = process_weights_argument(weights, verbose=verbose)
         self.__has_weights = weight_switch
         self.__weights = weights
+        self.__method = process_method_argument(method)
         self.__resolution = process_resolution_argument(resolution)
         self.__verbose = verbose
-        self.__is_finished = False
+        self.__kde_is_calculated = False
+        self.__entropy_is_calculated = False
+        self.__entropies = None
 
-    def calculate_legacy(self, verbose=None, ):
-        """
-        copy from kde module, to sneak a peak for me
-
-        """
-
-        if verbose:
-            print("Initializing C++ kernel for kde...")
-        k = kernel(self.data, self.resolution)
-        k.calculate()
-        self.set_is_finished(True)
-        if verbose:
-            print("KDE finished.")
-        self.set_pdf_x(center_grid(k.get_grid()))
-        self.set_bandwidth(k.get_bandwidth())
-        self.set_pdf(k.get_pdf())
-
-    def calculate(self, dihedralArr, resolution=4096, method="Simpson", verbose=False):
-        """Calculate the dihedral entropy of a trajectory.
-
+    def calculate(self, resolution=4096, method=None, verbose=None, id_gas=8.314):
+        """Calculate the dihedral entropy of a set of dihedrals.
+        # TODO Docstring
         The dihedral entropy of a number of different dihedral angles can be calculated using this
         function. The output will be a number for the entropy in each direction. This is calculated
         using a generalized cross entropy method, published by Y.Botev et al.
@@ -98,9 +90,6 @@ class dihedralEntropy(object):
 
         Parameters
         ----------
-        dihedralArr: list(list(float)) or list(float)
-            A 2D array holding all the dihedrals of a simulation (number Atoms,
-            number Dihedrals)
         resolution: int, optional
             The resolution for the estimation of the probability density function.
             This value is applied on the mirrored data. Therefore the resolution in
@@ -110,7 +99,9 @@ class dihedralEntropy(object):
         method: str, optional
             The method for the numerical integration scheme. Can be one of
             "Riemann" or "Simpson" (default is "Simpson")
-
+        verbose: bool
+        id_gas: float
+            ideal gas constants in J/(mol*K)
         Returns
         -------
         list:
@@ -122,29 +113,44 @@ class dihedralEntropy(object):
             if verbose:
                 print("Using resolution of {}".format(new_res))
             self.set_resolution(new_res)
-        method = process_method_argument(method)
+        method = method or process_method_argument(method)
         if verbose:
             print("Using the following method for integration: {}.".format(method))
 
+        if verbose:
+            print("Initializing C++ kernel for kde...")
         if self.has_weights:
             k = kernel_weights(self.data, self.weights, self.resolution)
+            if verbose:
+                print("Weights have been given for the calculation of the histograms.")
         else:
             k = kernel(self.data, self.resolution)
         k.calculate()
-        # integrate and calculat entropy from that...
+        self.set_kde_is_calculated(True)
+        if verbose:
+            print("KDE finished.")
+        self.set_pdf_x(k.get_grid())
+        self.set_bandwidth(k.get_bandwidth())
+        self.set_pdf(k.get_pdf())
+        start, end = start_end_from_grid(self.pdf_x)
+        integral = k.integrate(start, end, method=method)
+        entropies = integral * id_gas
+        self.set_entropies(entropies)
+        self.set_entropy_is_calculated(True)
+        ### old
+        # values = []
+        #
+        # if isinstance(dihedralArr[0], float):
+        #     dihedralArr = [dihedralArr]
+        #
+        # for dihedrals in dihedralArr:
+        #     # The mirroring is not necessary, all of this is done via the ent module
+        #     entropyCalculator = ent(list(dihedrals), resolution, method)
+        #     values.append(entropyCalculator.getResult() * -1)
+        #
+        # return values
 
-        # old
-        values = []
-
-        if isinstance(dihedralArr[0], float):
-            dihedralArr = [dihedralArr]
-
-        for dihedrals in dihedralArr:
-            # The mirroring is not necessary, all of this is done via the ent module
-            entropyCalculator = ent(list(dihedrals), resolution, method)
-            values.append(entropyCalculator.getResult() * -1)
-
-        return values
+        return entropies
 
     # Getter #
     def get_resolution(self):
@@ -162,23 +168,34 @@ class dihedralEntropy(object):
     def get_data(self):
         return self.__data
 
-    def get_is_finished(self):
+    def get_kde_is_calculated(self):
+        return self.__kde_is_calculated
+
+    def get_method(self):
         return self.__is_finished
 
     def get_pdf_x(self):
-        if not self.is_finished:
+        if not self.kde_is_calculated:
             self.calculate()
         return self.__pdf_x
 
     def get_bandwidth(self):
-        if not self.is_finished:
+        if not self.kde_is_calculated:
             self.calculate()
         return self.__bandwidth
 
     def get_pdf(self):
-        if not self.is_finished:
+        if not self.kde_is_calculated:
             self.calculate()
         return self.__pdf
+
+    def get_entropies(self):
+        if not self.entropy_is_calculated:
+            self.calculate()
+        return self.__entropies
+
+    def get_entropy_is_calculated(self):
+        return self.__entropy_is_calculated
 
     # setter #
     def set_resolution(self, value):
@@ -200,8 +217,11 @@ class dihedralEntropy(object):
         print("Data cannot be changed after initialization...")
         pass
 
-    def set_is_finished(self, value):
-        self.__is_finished = value
+    def set_method(self, value):
+        self.__method = value
+
+    def set_kde_is_calculated(self, value):
+        self.__kde_is_calculated = value
 
     def set_pdf_x(self, value):
         self.__pdf_x = value
@@ -212,14 +232,25 @@ class dihedralEntropy(object):
     def set_pdf(self, value):
         self.__pdf = value
 
+    def set_entropies(self, value):
+        self.__entropies = value
+
+    def set_entropy_is_calculated(self, value):
+        self.__entropy_is_calculated = value
+
     resolution = property(get_resolution, set_resolution, None, "resolution for kde")
     verbose = property(get_verbose, set_verbose, None, "Extend of print messages")
     data = property(get_data, set_data, None, "Data to do kde on.")
     weights = property(get_weights, set_weights, None, "Weights for the reweighting.")
     has_weights = property(get_has_weights, set_has_weights, None, "Flag, whether weights for the reweighting "
                                                                    "have been given for initialization.")
-    is_finished = property(get_is_finished, set_is_finished, None, "Data to do kde on.")
+    kde_is_calculated = property(get_kde_is_calculated, set_kde_is_calculated, None, "Has the kde calculation "
+                                                                                     "already been done?")
+    entropy_is_calculated = property(get_entropy_is_calculated, set_entropy_is_calculated, None, "Has the entropy "
+                                                                                                 "calculation already "
+                                                                                                 "been done?")
     bandwidth = property(get_bandwidth, set_bandwidth, None, "bandwidth")
-    pdf_x = property(get_pdf_x, set_pdf_x, None, "Pprobability densite sxfvsj_x ")
-    pdf = property(get_pdf, set_pdf, None, "Pprobability densite sxfvsj ")
-
+    pdf_x = property(get_pdf_x, set_pdf_x, None, "Grid for probability density function.")
+    pdf = property(get_pdf, set_pdf, None, "Probability density function.")
+    method = property(get_method, set_method, None, "Method for integrating the probability density function.")
+    entropies = property(get_entropies, set_entropies, None, "Calculated entropies for the data sets.")
